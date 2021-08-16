d1f174ce651cece182f0214785fb7cac85dc2a5b
Pablo Hariel <pablohariel@System-Product-Name-fd322bce.localdomain>
2021-08-16 20:42:47 +0000

    Exercises 3.7 - 3.8

--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
-import express from 'express'
+import express, { request, response } from 'express'
+import morgan, { token } from 'morgan'
 
 import { routes } from './routes/index.js'
 
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', (request, response) => {
+  return request.method === 'POST' ? JSON.stringify(request.body) : ''
+})
+
+app.use(morgan((tokens, request, response) => {
+  return [
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens.res(request, response, 'content-length'), '-',
+    tokens['response-time'](request, response), 'ms',
+    tokens.body(request, response)
+  ].join(' ')
+}))
+
 app.use(routes)
 
 app.listen(3001, () => console.log('Server up at port 3001'))
